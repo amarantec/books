7ee4110025c66e535c63866a8197ed65383af656
amarantec <marcoscarvalhop@outlook.com>
2024-07-23 22:36:43 -0300

    fix web

--- www/public/js/books/listBooks.js
+++ www/public/js/books/listBooks.js
@@ -2,8 +2,9 @@ export async function listBooks() {
   try {
     const response = await fetch('http://192.168.2.22:8080/list-books');
     const books = await response.json();
-    const booksList = document.getElementById('bookList');
-    booksList.innerHTML = '';
+    const content = document.getElementById('content');
+    
+    booksList.innerHTML = '<h2>List of Books</h2>';
     books.forEach(book => {
       const bookElement = document.createElement('div');
       bookElement.innerHTML = `<h3>${book.title}</h3><p>${book.description}</p>`;
