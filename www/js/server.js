f1311a6711565d080284f5b53af24a42151f6488
amarantec <marcoscarvalhop@outlook.com>
2024-07-26 12:56:44 -0300

    js user signup && login

--- www/js/server.js
+++ www/js/server.js
@@ -11,6 +11,7 @@ app.use('/', express.static(path.join(__dirname, '../html')));
 app.use('/js', express.static(path.join(__dirname, '')));
 app.use('/css', express.static(path.join(__dirname, '../css')));
 
+// GET all books
 app.get('/api/books', async (req, res) => {
   try {
     const response = await axios.get('http://192.168.2.22:8080/list-books');
@@ -20,6 +21,10 @@ app.get('/api/books', async (req, res) => {
   }
 });
 
+app.get('/books', (req, res) => {
+  res.sendFile(path.join(__dirname, '../html', 'books.html'));
+});
+// GET by Id
 app.get('/api/book/:id', async (req, res) => {
   try {
     const response = await axios.get(`http://192.168.2.22:8080/get-book/${req.params.id}`);
@@ -29,16 +34,38 @@ app.get('/api/book/:id', async (req, res) => {
   }
 });
 
-
-app.get('/books', (req, res) => {
-  res.sendFile(path.join(__dirname, '../html', 'books.html'));
-});
-
 app.get('/book/:id', (req, res) => {
   res.sendFile(path.join(__dirname, '../html', 'book.html'));
 });
 
+// POST Sign Up User
+app.post('/api/user-signup', async (req, res) => {
+   try {
+      const { name, email, password } = req.body;
+      const response = await axios.post('http://192.168.2.22:8080/user-signup', { name, email, password }
+    } catch (e) {
+        res.status(e.response?.status || 500).send(e.message);
+    }
+});
 
+app.get('/user-signup', (req, res) => {
+   res.sendFile(path.join(__dirname, '../html', 'signup,html'));  
+});
+
+
+// POST Login User
+app.post('/api/user-login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const response = await axios.post('http://192.168.2.22:8080/user-login', { email, password }
+  } catch (e) {
+      res.status(e.response?.status || 500).send(e.message)
+  }
+});
+
+app.get('/user-login', (req, res) => {
+  res.sendFile(path.join(__dirname, '../html', 'login.html'));
+});
 
 app.listen(port, () => {
   console.log(`API node.js rodando na porta ${port}`);
