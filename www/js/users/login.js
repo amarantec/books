5ba6bf4872b9b6d903b61b238e29ada6a5b7d8d5
amarantec <marcoscarvalhop@outlook.com>
2024-07-26 16:52:49 -0300

    login dom

---
+++ www/js/users/login.js
@@ -0,0 +1,27 @@
+document.getElementById('login-form').addEventListener('submit', async (event) => {
+	event.preventDefault(); // Evita o envio padrão do formulário
+
+	const email = document.getElementById('email').value;
+	const password = document.getElementById('password').value;
+
+	try {
+		const response = await fetch('/api/login', {
+			method: 'POST',
+			headers: {
+				'Cotent-Type': 'application/json'
+			},
+			body: JSON.stringify({ email, password })
+		});
+		
+		if (response.ok) {
+			const data = await response.json();
+			localStorage.setItem('authToken', data.token);
+			window.location.href = '/';
+		} else {
+			const error = await response.text();
+			alert(`Erro: ${error}`);
+		}
+	} catch (e) {
+			alert(`Error: ${e.message}`);
+	}
+});
---
+++ www/js/users/signup.js
@@ -0,0 +1,6 @@
+document.getElementById('login-form').addEventListener('submit', async (event) => {
+	event.preventDefault();
+	
+	const email = document.getElementById('username').value;
+	const 
+});
